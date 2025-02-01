814db14027e2511ba79c0216e9a8402af3e146bb
Ram Murmu <runashInc@outlook.com>
2025-02-01 21:49:13 +0530

    Update and rename Frontend/public to Fronted/src/app.js
    Add an src/app.js

---
+++ Fronted/src/app.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import CreatePost from './components/CreatePost';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/create-post" component={CreatePost} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
